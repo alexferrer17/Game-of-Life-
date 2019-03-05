67d858c02fe5f0cc6358f0be5da21708685a59a6
alexferrer17 <36723243+alexferrer17@users.noreply.github.com>
2019-03-05 10:23:39 -0800

    Add files via upload

---
+++ Mode.h
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Mode
+{
+private:
+
+public:
+
+    runModes();
+    ~runModes();
+
+    string mode;
+    int numOfGenerations = 0;
+    int population = 0;
+    int columns;
+    int rows;
+    int maxColumn;
+    int maxRow;
+    char a[1000][1000]; //first grid
+    char b[1000][1000]; //second grid
+
+    //look into what variables to declare
+
+    void compareGrids();
+    void randomConfig();
+    void printGrid();
+    void copyGeneration();
+    void controlPopulation();
+    int NeighbordCheck(int, int);
+    int NeighbordCheckDonought(int, int);
+    int NeighbordCheckMirror(int, int);
+    void providegrid(); //provide grid from file
+
+
+
+
+};
